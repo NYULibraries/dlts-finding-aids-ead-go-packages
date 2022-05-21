0fefceba9da08f9c92bf5252d657049d54729327
David <da70@nyu.edu>
2022-05-20 20:57:42 -0400

    validate.go: add new make error message functions for invalid eadid, repository name, and relator codes

--- ead/validate/validate.go
+++ ead/validate/validate.go
@@ -1,6 +1,10 @@
 package validate
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
 
 func ValidateEAD(ead []byte) []string {
 	return []string{
@@ -16,4 +20,44 @@ func makeInvalidXMLErrorMessage() string {
 
 func makeMissingRequiredElementErrorMessage(elementName string) string {
 	return fmt.Sprintf("Required element %s not found.", elementName)
+}
+
+func makeInvalidEADIDErrorMessage(eadid string, invalidCharacters []byte) string {
+	return fmt.Sprintf(`Invalid <eadid>
+
+<eadid> value "%s" does not conform to the Finding Aids specification.
+There must be between 2 to 8 character groups joined by underscores.
+The following characters are not allowed in character groups: %s
+`, eadid, string(bytes.Join([][]byte{invalidCharacters}, []byte(" "))))
+}
+
+func makeInvalidRepositoryErrorMessage(repositoryName string) string {
+	return fmt.Sprintf(`Invalid <repository>
+
+	<repository> contains unknown repository name "%s".
+		The repository name must match a value from this list:
+
+Akkasah: Center for Photography (NYU Abu Dhabi)
+	New York University Archives
+	Center for Brooklyn History
+	Fales Library and Special Collections
+	Villa La Pietra
+	New-York Historical Society
+	NYU Abu Dhabi, Archives and Special Collections
+	Poly Archives at Bern Dibner Library of Science and Technology
+	Tamiment Library and Robert F. Wagner Labor Archives`, repositoryName)
+}
+
+func makeUnrecognizedRelatorCodesErrorMessage(unrecognizedRelatorCodes [][]string) string {
+	var unrecognizedRelatorCodeSlice []string
+	for _, elementAttributePair := range unrecognizedRelatorCodes {
+		unrecognizedRelatorCodeSlice = append(unrecognizedRelatorCodeSlice,
+			fmt.Sprintf(`%s has role="%s"`, elementAttributePair[0], elementAttributePair[1]))
+	}
+
+	return fmt.Sprintf(`Unrecognized relator codes
+
+The EAD file contains elements with role attributes containing unrecognized relator codes:
+
+%s`, strings.Join(unrecognizedRelatorCodeSlice, "\n"))
 }
