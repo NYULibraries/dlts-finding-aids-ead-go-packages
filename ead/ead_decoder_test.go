b7f0f62b136f79fbcd476d3f261fa78e0a1532f7
jgpawletko <jgpawletko@gmail.com>
2023-01-17 17:16:04 -0500

    change from ioutil to os

--- ead/ead_decoder_test.go
+++ ead/ead_decoder_test.go
@@ -3,7 +3,7 @@ package ead
 import (
 	"encoding/json"
 	"encoding/xml"
-	"io/ioutil"
+	"os"
 	"path/filepath"
 	"testing"
 )
@@ -22,14 +22,14 @@ func TestNoteChildOrder(t *testing.T) {
 		// reference file includes newline at end of file so
 		// add newline to jsonData
 		jsonData = append(jsonData, '\n')
-		if err := ioutil.WriteFile(filepath.Join(testFixtureDataPath, "omega-ead-test-order.json"), jsonData, 0755); err != nil {
+		if err := os.WriteFile(filepath.Join(testFixtureDataPath, "omega-ead-test-order.json"), jsonData, 0755); err != nil {
 			t.Error(err)
 		}
 	})
 }
 
 func getOrderXMLOmega(t *testing.T) EAD {
-	EADXML, err := ioutil.ReadFile(filepath.Join(testFixtureDataPath, "Omega-EAD.xml"))
+	EADXML, err := os.ReadFile(filepath.Join(testFixtureDataPath, "Omega-EAD.xml"))
 	if err != nil {
 		t.Error(err)
 	}
