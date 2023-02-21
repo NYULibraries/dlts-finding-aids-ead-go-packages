5780aba6ed5bbc1365d7bf6856bfd4ebcc19d1ca
jgpawletko <jgpawletko@gmail.com>
2023-02-20 20:08:14 -0500

    update version timestamp

--- ead/ead.go
+++ ead/ead.go
@@ -5,7 +5,7 @@ package ead
 // Based on: "Data model for parsing EAD <archdesc> elements": https://jira.nyu.edu/jira/browse/FADESIGN-29.
 
 const (
-	Version = "v0.18.1"
+	Version = "v0.18.1-20230220T20_05"
 )
 
 type EAD struct {
