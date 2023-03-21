4b349159c0b331f92a29ac181a36e9602358ae6c
jgpawletko <jgpawletko@gmail.com>
2023-03-20 20:51:53 -0400

    add embedded presentation element decoder tests

--- ead/ead_decoder_test.go
+++ ead/ead_decoder_test.go
@@ -28,8 +28,67 @@ func TestNoteChildOrder(t *testing.T) {
 	})
 }
 
+func TestMinimalStreamParsingEmbeddedPresentationElements(t *testing.T) {
+
+	t.Run("Test Minimal Stream-Parsing and JSON Marshaling of Embedded Presentation Elements", func(t *testing.T) {
+		xmlSnippet, err := os.ReadFile(filepath.Join(testFixtureDataPath, "materialspec.xml"))
+		if err != nil {
+			t.Error(err)
+		}
+		var child EADChild
+		err = xml.Unmarshal([]byte(xmlSnippet), &child)
+		if err != nil {
+			t.Error(err)
+		}
+
+		jsonData, err := json.MarshalIndent(child, "", "    ")
+		if err != nil {
+			t.Error(err)
+		}
+
+		// reference file includes newline at end of file so
+		// add newline to jsonData
+		jsonData = append(jsonData, '\n')
+		if err := os.WriteFile(filepath.Join(testFixtureDataPath, "minimal-stream-parsing-with-embedded-presentation-elements.json"), jsonData, 0755); err != nil {
+			t.Error(err)
+		}
+	})
+}
+
+func TestStreamParsingEmbeddedPresentationElements(t *testing.T) {
+	t.Run("Test Stream-Parsing and JSON Marshaling Embedded Presentation Elements", func(t *testing.T) {
+		ead := getEmbeddedPresentationElementsEAD(t)
+		//ead := getOrderXMLOmega(t)
+		jsonData, err := json.MarshalIndent(ead, "", "    ")
+		if err != nil {
+			t.Error(err)
+		}
+
+		// reference file includes newline at end of file so
+		// add newline to jsonData
+		jsonData = append(jsonData, '\n')
+		if err := os.WriteFile(filepath.Join(testFixtureDataPath, "embedded-presentation-elements.json"), jsonData, 0755); err != nil {
+			t.Error(err)
+		}
+	})
+}
+
 func getOrderXMLOmega(t *testing.T) EAD {
 	EADXML, err := os.ReadFile(filepath.Join(testFixtureDataPath, "Omega-EAD.xml"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	var ead EAD
+	err = xml.Unmarshal([]byte(EADXML), &ead)
+	if err != nil {
+		t.Error(err)
+	}
+	return ead
+}
+
+func getEmbeddedPresentationElementsEAD(t *testing.T) EAD {
+	EADXML, err := os.ReadFile(filepath.Join(testFixtureDataPath, "mss_293.xml"))
 	if err != nil {
 		t.Error(err)
 	}
