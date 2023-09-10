728fb961a911a4cf340fe7f5c4e0b463dac098af
jgpawletko <jgpawletko@gmail.com>
2023-09-10 16:45:43 -0400

    rename fixtures directory

--- ead/modify/modify.go
+++ ead/modify/modify.go
@@ -7,38 +7,76 @@ import (
 	"github.com/lestrrat-go/libxml2/xpath"
 )
 
-func ModifyEAD(data []byte) []string {
+func ModifyEAD(data *[]byte) (string, []string) {
 
 	var errors = []string{}
 
 	p := parser.New()
-	doc, err := p.Parse(data)
+	doc, err := p.Parse(*data)
 	if err != nil {
 		errors = append(errors, "Unable to parse XML file")
-		return append(errors, err.Error())
+		return "", append(errors, err.Error())
 	}
 	defer doc.Free()
 
 	root, err := doc.DocumentElement()
 	if err != nil {
 		errors = append(errors, "Unable to extract root node")
-		return append(errors, err.Error())
+		return "", append(errors, err.Error())
 	}
 
 	ctx, err := xpath.NewContext(root)
 	if err != nil {
 		errors = append(errors, "Unable to initialize XPathContext")
-		return append(errors, err.Error())
+		return "", append(errors, err.Error())
 	}
 	defer ctx.Free()
 
-	exprString := `//origination[@label]`
+	prefix := `_`
+	nsuri := `urn:isbn:1-931666-22-9`
+	if err := ctx.RegisterNS(prefix, nsuri); err != nil {
+		errors = append(errors, "Failed to register namespace")
+		return "", append(errors, err.Error())
+	}
+	exprString := `//_:origination/@label[.='Creator']`
 	nodes := xpath.NodeList(ctx.Find(exprString))
 
 	for _, n := range nodes {
-		fmt.Println(n)
+		n.SetNodeValue("creator")
+	}
+
+	//	exprString = `//_:container`
+	//	exprString = `//_:container[@id and @parent]`
+	exprString = `//_:container[@id and not(@parent)]/@id`
+	nodes = xpath.NodeList(ctx.Find(exprString))
+
+	for _, n := range nodes {
+		rootID := n.NodeValue()
+		fmt.Printf("root: %s\n", rootID)
+		updateSubContainer(ctx, rootID, rootID)
 	}
 
 	// all ok, return empty slice
-	return []string{}
+	return doc.String(), []string{}
+}
+
+func updateSubContainer(ctx *xpath.Context, parentID string, rootID string) {
+	// find the ID nodes of all containers whose @parent attribute value == parentID
+	exprString := fmt.Sprintf("//_:container[@parent = \"%s\"]/@id", parentID)
+	nodes := xpath.NodeList(ctx.Find(exprString))
+
+	// recursively process all containers
+	for _, n := range nodes {
+		// get the value of this @id node
+		id := n.NodeValue()
+		// update any subcontainers for whom this container is a parent
+		updateSubContainer(ctx, id, rootID)
+
+		// recursive calls are complete
+		// update this node
+		containerNode, _ := n.ParentNode()
+		parentAttributeNode, _ := containerNode.Find(`./@parent`)
+		// set the parent attribute to the rootID value
+		parentAttributeNode.NodeList().First().SetNodeValue(rootID)
+	}
 }
