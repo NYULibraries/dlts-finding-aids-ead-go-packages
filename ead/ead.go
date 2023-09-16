fa40b57357393e431e712b7dd7ab1847c2ed8dac
jgpawletko <jgpawletko@gmail.com>
2023-09-15 21:23:30 -0400

    bump version to v0.28.0
    Remove -pre.* suffix from package version
    Update CHANGELOG to reflect release version

--- ead/ead.go
+++ ead/ead.go
@@ -5,7 +5,7 @@ package ead
 // Based on: "Data model for parsing EAD <archdesc> elements": https://jira.nyu.edu/jira/browse/FADESIGN-29.
 
 const (
-	Version = "v0.28.0-pre.1"
+	Version = "v0.28.0"
 )
 
 type EAD struct {
